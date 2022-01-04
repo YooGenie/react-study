99db6978fe57f6c7aba3d3a705be1df85aee280a
ysun7 <ysun7@beautifulstore.org>
2022-01-04 22:47:04 +0900

    #20220104 create-react-app 설치

---
+++ README.md
@@ -0,0 +1,4 @@
+# **리액트 공부 노트**
+
+블로그 <br>
+https://pink1016.tistory.com/
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>공부하기</button>
+      <button>메뉴판</button>
+    </div>
+  );
+}
+
+export default App;
