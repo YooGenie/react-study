46e7ba6d4ea51bb010decc99850a35801a31ed3d
ysun7 <ysun7@beautifulstore.org>
2022-09-06 09:26:18 +0900

    #13 feat: PDF 미리보기 해보기

--- src/page/study/ViewPDFPage.js
+++ src/page/study/ViewPDFPage.js
@@ -1,12 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import {Button,message} from "antd";
 import {ApiService} from "./ApiService";
 import { saveAs } from "file-saver";
 
 const ViewPDF = () => {
+
+    // $http.post('/fetchBlobURL',{myParams}, {responseType: 'arraybuffer'})
+    //     .success(function (data) {
+    //         var file = new Blob([data], {type: 'application/pdf'});
+    //         var fileURL = URL.createObjectURL(file);
+    //         window.open(fileURL);
+    //     });
+
+
     const downloadPdf = (id) => {
         ApiService.getPdf(id)
             .then((bold) => {
+                const file = new Blob([bold.data], {type: 'application/pdf'});
+                const fileURL = URL.createObjectURL(file);
+                window.open(fileURL);
+                console.log(file)
                 saveAs(bold.data, "member_list.pdf");
                 message.success("다운되었습니다")
                 const link = document.createElement("a");
@@ -18,9 +31,12 @@ const ViewPDF = () => {
     }
 
     return (
+        <>
         <div>
             <Button id="exportFile" onClick={() => {downloadPdf(2)}}>PDF 다운</Button>
         </div>
+        <div>미리보기</div>
+        </>
     )
 }
 
