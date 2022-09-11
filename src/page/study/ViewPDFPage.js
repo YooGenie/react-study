a45d4eb34d6c0d74c6e853d8f5466f6867724ae9
ysun7 <ysun7@beautifulstore.org>
2022-09-11 22:51:40 +0900

    #20 feat: 클릭하면 PDF 미리볼 수 있는 모달창 열린다

--- src/page/study/ViewPDFPage.js
+++ src/page/study/ViewPDFPage.js
@@ -1,41 +1,53 @@
 import React, {useState,useEffect} from 'react';
-import {Button,message} from "antd";
+import {Button, message} from "antd";
 import {ApiService} from "./ApiService";
-import { saveAs } from "file-saver";
+import {saveAs} from "file-saver";
+import PreviewPDF from "./PreviewPDF";
 
 const ViewPDF = () => {
-
-    // $http.post('/fetchBlobURL',{myParams}, {responseType: 'arraybuffer'})
-    //     .success(function (data) {
-    //         var file = new Blob([data], {type: 'application/pdf'});
-    //         var fileURL = URL.createObjectURL(file);
-    //         window.open(fileURL);
-    //     });
-
-
+    const [file, setFile] = useState()
+    const [openModal, setOpenModal] = useState(false)
     const downloadPdf = (id) => {
         ApiService.getPdf(id)
             .then((bold) => {
                 const file = new Blob([bold.data], {type: 'application/pdf'});
+                setFile(file)
                 const fileURL = URL.createObjectURL(file);
                 window.open(fileURL);
-                console.log(file)
                 saveAs(bold.data, "member_list.pdf");
                 message.success("다운되었습니다")
-                const link = document.createElement("a");
-                link.click();
             })
             .catch(() => {
                 message.error("다운 실패했습니다")
             })
     }
 
+    const getPDF = (id) => {
+        ApiService.getPdf(id)
+            .then((bold) => {
+                const file = new Blob([bold.data], {type: 'application/pdf'});
+                const fileURL = URL.createObjectURL(file);
+                setFile(fileURL)
+            })
+    }
+
+    useEffect(()=>getPDF(2), [])
+
     return (
         <>
-        <div>
-            <Button id="exportFile" onClick={() => {downloadPdf(2)}}>PDF 다운</Button>
-        </div>
-        <div>미리보기</div>
+            <div>
+                <Button id="exportFile" onClick={() => {
+                    downloadPdf(2)
+                }}>PDF 다운</Button>
+            </div>
+            <div><Button id="preview" onClick={() => {
+                setOpenModal(true)
+            }}>PDF 미리보기</Button></div>
+            <PreviewPDF
+                openModal={openModal}
+                file={file}
+                onClose={() => setOpenModal(false)}
+            />
         </>
     )
 }
